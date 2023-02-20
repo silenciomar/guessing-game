7b555b3a37f54b61134e15dfe890bbdf328d9a67
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-02-20 09:34:50 +0600

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {};
 
     setRange(min, max) {
-
-    }
+        this.min = min;
+        this.max = max;
+    };
 
     guess() {
-
-    }
+        this.guessingNumber = Math.ceil((this.max + this.min) / 2);
+        return this.guessingNumber;
+    };
 
     lower() {
-
-    }
+        this.max = this.guessingNumber;
+    };
 
     greater() {
-
-    }
+        this.min = this.guessingNumber;
+    };
 }
 
 module.exports = GuessingGame;
